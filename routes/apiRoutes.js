25fa788b8bde41e1d987c46076a28fb26bc99439
Marian Akiwumi <mfakiwumi1992@yahoo.com>
2020-04-11 20:39:02 -0400

    Defined all routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 const db = require("../db/db.json");
 
-router.get("/notes", function(request, response) {
-  // get a note
-  // see 15-HotRestaurant/Solved for more info
-  response.send(db);
-});
+module.exports = router {
+  router.get("/api/notes", function(request, response) {
+    // get a note
+    // see 15-HotRestaurant/Solved for more info
+    response.send(db);
+  });
+}
 
 // other apis here
 
--- server.js
+++ server.js
@@ -1,16 +1,29 @@
-// basic server template
-// see 14-FinalStarwarsApp/server-template.js
-// plus require routes 
+//import dependances
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const PORT = process.env.PORT || 3000;
+let app = express();
 
-// PLUS the following 
+//declaring routes
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Create routes 
+
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+
+//Create paths to html, css
 app.use(express.static("public"));
-/* 
-^^ creates a path to static front-end files: html, css & js.
-notice how files are referenced in index.html: 
-                       <script src="/js/index.js"></script>. 
-this is because we have created a static path that routes
-to the /public directory automatically.
-*/
-app.use("/api", apiRoutes);  // /api/notes
-app.use("/", htmlRoutes);    // /notes or /
 
+
+//calling routes
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+//port listener
+app.listen(PORT, function() {
+console.log("http://localhost:" + PORT);
+});
