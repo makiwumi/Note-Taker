17686ae05171d307bf3ae0369382fdcae21540a8
Marian Akiwumi <mfakiwumi1992@yahoo.com>
2020-04-12 18:09:54 -0400

    Removed function around router

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,13 +1,8 @@
 let path = require("path");
 let router = require("express").Router();
 
-module.exports = function(app) {
-    router.get("/notes", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/notes.html"))
-    });
-}
-
-
-
+router.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"))
+});
 
 module.exports = router;
